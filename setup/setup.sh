5958aeea241649f5b056af99018b5acae5bf5b93
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2023-03-26 16:24:31 +0200

    init 2

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,3 +8,4 @@ sudo apt -qqq -o Dpkg::Use-Pty=0 install software-properties-common python3 pyth
 echo "export PATH=$PATH:$HOME/.local/bin" >> $HOME/.bashrc
 pip install --no-warn-script-location --user ansible
 # d92615e3fd2a83a68cfaa08a
+
